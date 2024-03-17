3c1ca1ff774dacbd2e5d9677999e3a604bf7a1f3
Ulfflo <145294663+Ulfflo@users.noreply.github.com>
2024-03-17 17:59:54 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
-import "@/styles/globals.css";
+import { useEffect } from "react";
+import { ThemeProvider, useTheme } from "../contexts/ThemeContext";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
+  const { state } = useTheme();
+
+  useEffect(() => {
+    const htmlTag = document.documentElement;
+    if (state.theme === "dark") {
+      htmlTag.classList.add("dark");
+    } else {
+      htmlTag.classList.remove("dark");
+    }
+  }, [state.theme]);
+
   return <Component {...pageProps} />;
 }
+
+export default ({ Component, pageProps }) => (
+  <ThemeProvider>
+    <MyApp Component={Component} pageProps={pageProps} />
+  </ThemeProvider>
+);
